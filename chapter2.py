24a4b030130ff0f52ce9e48332ef7b8635a5e4f9
sdoshi983 <sdoshi983@rku.ac.in>
2021-01-29 23:44:24 +0530

    Original image converted to gray image using cvtColor function

--- chapter2.py
+++ chapter2.py
@@ -1,1 +1,6 @@
 import cv2
+
+img = cv2.imread('resources/images.jpg')
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)     # cvtColor function stands for convertColor
+cv2.imshow('Gray Image', imgGray)
+cv2.waitKey(0)
