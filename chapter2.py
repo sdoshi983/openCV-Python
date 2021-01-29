268ca84189766834dff4adc7066c2890a7f94926
sdoshi983 <sdoshi983@rku.ac.in>
2021-01-30 00:08:26 +0530

    Canny image developed

--- chapter2.py
+++ chapter2.py
@@ -2,8 +2,12 @@ import cv2
 
 img = cv2.imread('resources/images.jpg')
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)     # cvtColor function stands for convertColor
-cv2.imshow('Gray Image', imgGray)
+# cv2.imshow('Gray Image', imgGray)
 
 imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 0)      # kSize parameter should be (odd,odd) only. For eg, (3,3), (5,5), (7,7)
-cv2.imshow('Blur Image', imgBlur)
+# cv2.imshow('Blur Image', imgBlur)
+
+
+imgCanny = cv2.Canny(img, 100, 100)     # Canny function is used to detect edges in our image
+cv2.imshow('Canny Image', imgCanny)
 cv2.waitKey(0)
