9b10279df337c6241c2c1a0a0d34b707db4efe6a
sdoshi983 <sdoshi983@rku.ac.in>
2021-01-29 15:12:42 +0530

    Video Capture implemented

---
+++ chapter1.py
@@ -0,0 +1,17 @@
+import cv2
+
+# FOR IMAGES
+# img = cv2.imread('resources/images.jpg')
+# cv2.imshow('Output',img)
+# cv2.waitKey(0)
+
+# FOR VIDEOS
+# cap = cv2.VideoCapture("resources/video.mp4")
+#
+# while True:
+#     success, img = cap.read()
+#     print(success)
+#     if success:
+#         cv2.imshow("Video", img)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
