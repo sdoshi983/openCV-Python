86cdc349f4a0272376f754ba244b3f44719f8cff
sdoshi983 <sdoshi983@rku.ac.in>
2021-02-01 16:48:41 +0530

    Resizing implemented

--- chapter3.py
+++ chapter3.py
@@ -1,4 +1,13 @@
 import cv2
+import numpy as np
 
 #       Resizing and cropping:
+img = cv2.imread('resources/images.jpg')
+print(img.shape)
+cv2.imshow('image',img)
 
+resizedImg = cv2.resize(img, (200, 175))
+print(resizedImg.shape)
+cv2.imshow('resized', resizedImg)
+
+cv2.waitKey(0)
