ac14c77609db18c15bfb8d3ed18a8986b533d40c
sdoshi983 <sdoshi983@rku.ac.in>
2021-02-01 22:19:45 +0530

    Line segment from one point to another point with specific color created

--- chapter4.py
+++ chapter4.py
@@ -1,4 +1,10 @@
 import cv2
-
+import numpy as np
 #           shapes and texts
 
+img = np.zeros((512, 512, 3), np.uint8)
+print(img.shape)
+# img[:] = 255, 0, 0      # BGR
+cv2.line(img, (50, 0), (50, 500), (0, 255, 0))
+cv2.imshow('image', img)
+cv2.waitKey(0)
