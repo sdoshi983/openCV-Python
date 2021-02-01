4977d7fdfdf98f6c9f982a2cc2ffde3a44705583
sdoshi983 <sdoshi983@rku.ac.in>
2021-02-01 23:22:15 +0530

    Warp perspective implemented and fifth chapter completed

--- chapter5.py
+++ chapter5.py
@@ -1,4 +1,13 @@
 import cv2
 import numpy as np
 
+#       warp perspective / bird view
+
 img = cv2.imread('resources/cards.jpg')
+width, height = 250, 350
+pts1 = np.float32([[111, 219], [287, 188], [154, 482], [352, 440]])     # four points that we want as a final output
+pts2 = np.float32([[0, 0], [width, 0], [0, height], [width,  height]])  # the size of the final output image
+matrix = cv2.getPerspectiveTransform(pts1, pts2)    # getting the matrix of the image with the help of points and size
+imgOutput = cv2.warpPerspective(img, matrix, (width, height))   # img, image matrix, size
+cv2.imshow('image', imgOutput)
+cv2.waitKey(0)
