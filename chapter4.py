d0867c36fbe4a6247cbdee0290d1141e21a3780f
sdoshi983 <sdoshi983@rku.ac.in>
2021-02-01 22:38:28 +0530

    Circle with specific center point and radius created

--- chapter4.py
+++ chapter4.py
@@ -7,6 +7,7 @@ img = np.zeros((512, 512, 3), np.uint8)
 print(img.shape)
 # img[:] = 255, 0, 0      # BGR
 cv2.line(img, (0, 0), (img.shape[1], img.shape[0]), (0, 255, 0), 5)    # img, start point, end point, color, thickness
-cv2.rectangle(img, (10, 10), (100, 100), (255, 0, 0), cv2.FILLED)
+cv2.rectangle(img, (10, 10), (100, 100), (255, 0, 0), cv2.FILLED)       # same as above
+cv2.circle(img, (300, 200), 30, (0, 0, 255), 10)       #img, center, radius, color, thickness
 cv2.imshow('image', img)
 cv2.waitKey(0)
