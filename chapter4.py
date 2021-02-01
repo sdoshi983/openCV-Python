0ce0a6a460d21d2c60320d8633eb3614e2689b00
sdoshi983 <sdoshi983@rku.ac.in>
2021-02-01 22:32:26 +0530

    Filled rectangle created

--- chapter4.py
+++ chapter4.py
@@ -2,9 +2,11 @@ import cv2
 import numpy as np
 #           shapes and texts
 
+#shapes
 img = np.zeros((512, 512, 3), np.uint8)
 print(img.shape)
 # img[:] = 255, 0, 0      # BGR
-cv2.line(img, (50, 0), (50, 500), (0, 255, 0))
+cv2.line(img, (0, 0), (img.shape[1], img.shape[0]), (0, 255, 0), 5)    # img, start point, end point, color, thickness
+cv2.rectangle(img, (10, 10), (100, 100), (255, 0, 0), cv2.FILLED)
 cv2.imshow('image', img)
 cv2.waitKey(0)
