4f05e53896d70beb4e351163dcf27b0d602997a1
sdoshi983 <sdoshi983@rku.ac.in>
2021-02-02 16:19:01 +0530

    Vertical stack of numpy used

--- chapter6.py
+++ chapter6.py
@@ -1,7 +1,12 @@
 import cv2
 import numpy as np
 
+#       Joining images
+
 img = cv2.imread('resources/images.jpg')
-imgHorizontal = np.hstack((img, img))
+imgHorizontal = np.hstack((img, img))   # hstack stands for horizontal stack and it accepts a tuple and the elements of tuples are children of stack which are placed horizontally
 cv2.imshow('horizontal', imgHorizontal)
+
+imgVertical = np.vstack((img, img))
+cv2.imshow('vertical', imgVertical)     # here, the elements are stacked vertically
 cv2.waitKey(0)
