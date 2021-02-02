c938734acce5b10dfe2ebc28d8606c893fac1acf
sdoshi983 <sdoshi983@rku.ac.in>
2021-02-02 22:52:34 +0530

    HSV image generated

--- chapter7.py
+++ chapter7.py
@@ -3,3 +3,10 @@ import numpy as np
 
 #           Color detection
 
+
+img = cv2.imread('resources/lambo.png')
+
+imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)   # as such the range of HSV is 0-360 but its 0-179 in Open CV
+cv2.imshow('original', img)
+cv2.imshow('HSV', imgHSV)
+cv2.waitKey(0)
