37a4b5c53ac7f4540aeb208dad9212c2306db792
sdoshi983 <sdoshi983@rku.ac.in>
2021-02-02 17:32:21 +0530

    Solution function for drawbacks of numpy stack started

--- chapter6.py
+++ chapter6.py
@@ -8,9 +8,20 @@ imgHorizontal = np.hstack((img, img))   # hstack stands for horizontal stack and
 cv2.imshow('horizontal', imgHorizontal)
 
 imgVertical = np.vstack((img, img))
-cv2.imshow('vertical', imgVertical)     # here, the elements are stacked vertically
+cv2.imshow('vertical', imgVertical) # here, the elements are stacked vertically
 cv2.waitKey(0)
 
 # NOTE:
 #   - the issue with the above method is that all the elements of the tuple should have same BGR else it will not work
 #   - the images cannot be resized. If we want to stack multiple elements then we can't assign them our specific size. The size will be taken by default
+
+# the solution to above two points is:
+
+def stackImages(scale, imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)   # isinstance returns true is imgArray[0] is a type of list, else returns false
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+
+
