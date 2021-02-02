6bd2d78fcf307e22fa511d8cb93d93127db9270c
sdoshi983 <sdoshi983@rku.ac.in>
2021-02-02 23:09:20 +0530

    New window created

--- chapter7.py
+++ chapter7.py
@@ -3,8 +3,11 @@ import numpy as np
 
 #           Color detection
 
-
+def empty(a):
+    pass
 img = cv2.imread('resources/lambo.png')
+cv2.namedWindow('TrackBars')
+cv2.resizeWindow('TrackBars', 640, 240)
 
 imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)   # as such the range of HSV is 0-360 but its 0-179 in Open CV
 cv2.imshow('original', img)
