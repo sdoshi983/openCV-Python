872f7ce4b2f0c0a80c468aa8fbb4579332765197
sdoshi983 <sdoshi983@rku.ac.in>
2021-02-03 16:04:15 +0530

    All the contours present in the original image drawn, hence all the shapes detected

--- chapter8.py
+++ chapter8.py
@@ -3,44 +3,55 @@ import numpy as np
 
 #           Contours / shape detection
 
-def stackImages(scale, imgArray):
+def stackImages(scale,imgArray):
     rows = len(imgArray)
     cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)   # isinstance returns true is imgArray[0] is a type of list, else returns false
+    rowsAvailable = isinstance(imgArray[0], list)
     width = imgArray[0][0].shape[1]
     height = imgArray[0][0].shape[0]
     if rowsAvailable:
-        for x in range(0, rows):
+        for x in range ( 0, rows):
             for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == img[0][0].shape[:2]:
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
                     imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
                 else:
                     imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-
-                if len(imgArray[x][y].shape) == 2:      # if it is a gray image then converting it to BGR
-                    imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
-
-        imageBlank = np.zeros((height, width, 3), np.uint8)     # creating a matrix with all zeros
-        horizontal = [imageBlank] * rows
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        # hor_con = [imageBlank]*rows
         for x in range(0, rows):
-            horizontal[x] = np.hstack(imgArray[x])      # stacking images horizontally
-        vertical = np.vstack(horizontal)        # stacking all the horizontally stacked images (rows) to vertically (columns)
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
     else:
         for x in range(0, rows):
             if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
                 imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
             else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
-            if len(imgArray[x].shape) == 2:
-                imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        horizontal = np.hstack(imgArray)
-        vertical = horizontal
-    return vertical
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
 
+
+def getContours(img):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        print(area)
+        cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
 
 img = cv2.imread('resources/shapes.png')
+imgContour = img.copy()
+
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 1)
-imgStack = stackImages(0.6, ([img, imgGray, imgBlur]))
+imgCanny = cv2.Canny(imgBlur, 50, 50)
+imgBlank = np.zeros_like(img)
+
+getContours(imgCanny)
+imgStack = stackImages(0.6, ([img, imgGray, imgBlur],
+                             [imgCanny, imgContour, imgBlank]))
 cv2.imshow('stack', imgStack)
 cv2.waitKey(0)
